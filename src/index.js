f036d7a5c89db3baa1637ecd9620eacfef4d0e77
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-03 00:12:17 +0000

    form multiple input fields

--- src/index.js
+++ src/index.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import Gallery from "./Kodiri";
 import App from "./App";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-function Car(props) {
-  return <li>{props.model}</li>
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    console.log('event', event)
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('inputs', inputs);
+    alert(inputs);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor=''>
+        Enter your name:
+        <input
+          type='text'
+          name='username'
+          value={inputs.username || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Enter your age:
+        <input
+          type='number'
+          name='age'
+          value={inputs.age || ""}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit"/>
+    </form>
+  );
 }
 
-function Garage() {
-  const cars = [
-    {id:1, brand: 'Ford'},
-    {id:2, brand: 'LandRover'},
-    {id:3, brand: 'Fiat'},
-    {id:4, brand: 'Reanulat'},
-  ]
-  return(
-    <div>
-     {cars.map(car => {
-       return <li key={car.id}>{car.brand}</li>
-     })} 
-    </div>
-  )
-}
-
-ReactDOM.render(
-  <Garage/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<MyForm />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-if(module.hot) {
+if (module.hot) {
   module.hot.accept();
 }
