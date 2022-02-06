552bedd842d82660b141b0826f8f0fb2a90eb0f2
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-06 12:48:15 +0000

    creating new elements by pure js, building by fn

--- src/plainTODOapp/controller.js
+++ src/plainTODOapp/controller.js
@@ -9,5 +9,34 @@ function appReady() {
 }
 
 function renderTodos() {
-    
+    let rootNode = document.querySelector('#todoList');
+    model.todos.forEach(todo => {
+        rootNode.appendChild(getTodoNode(todo));
+    });
+}
+
+function getTodoNode(todo) {
+    let todoNode = document.createElement('div');
+    todoNode.setAttribute(
+        'class', 
+        `todo-item ${todo.isChecked ? 'is-checked' : ''}`);
+    todoNode.appendChild(getTodoCheckboxNode(todo));
+    todoNode.appendChild(getTodoLabelNode(todo));
+    //todoNode.innerText = todo.text;
+    return todoNode;
+}
+
+function getTodoCheckboxNode(todo) {
+    let checkboxNode = document.createElement('input');
+    checkboxNode.setAttribute('type', 'checkbox');
+    if(todo.isChecked) {
+        checkboxNode.setAttribute('checked', '');
+    }
+    return checkboxNode;
+}
+
+function getTodoLabelNode(todo) {
+    let labelNode = document.createElement('label');
+    labelNode.innerText = todo.text;
+    return labelNode;
 }
